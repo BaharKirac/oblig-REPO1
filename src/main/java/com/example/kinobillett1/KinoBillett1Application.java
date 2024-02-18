e841e099fcd69e545d23d84408efc84784f7c63d
BaharKrc <144059055+BaharKrc@users.noreply.github.com>
2024-02-18 19:06:08 +0100

    NEW

---
+++ src/main/java/com/example/kinobillett1/KinoBillett1Application.java
@@ -0,0 +1,13 @@
+package com.example.kinobillett1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoBillett1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoBillett1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kinobillett1/KinoBillett1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinobillett1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBillett1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
